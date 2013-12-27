0f57eca0dc180cd0412f96110d678654f1c2dd17
Louis-Ye <yeluyupt@gmail.com>
2013-12-27 12:21:08 +0800

    put FibNode into FibonacciHeap

--- README.md
+++ README.md
@@ -3,14 +3,15 @@ FibonacciHeap
 <b>Version 0.01</b> <br>
 
 1. A C++ library which offers Fibonacci Heap data structure and corresponding operations.
-2. The interfaces of this library are much like the ones of piority queue in STL.
-3. Comparison depending on the operator < .
-4. <b>LICENSE</b>: MIT
-5. Usage:
-
+2. <b>LICENSE</b>: MIT
+3. Interfaces: The interfaces of this library are much like the ones of piority queue in STL.
+4. Comparison: depends on the operator < .
+5. Decrease Key: In order to do so, you have to maintain the pointer of each node by yourself.
+6. Creating new node: The <i>push(T key)</i> method will return the pointer and give the new node an integer ID according to the pushing sequence. You can also set the ID for each new node in your own purpose with the <i>push(T key, int id)</i> method.
+7. Usage:
 ````cpp
-#include "fibonacciheap.h"
 #include <iostream>
+#include "fibonacciheap.h"
 
 using namespace std;
 
@@ -18,19 +19,26 @@ using namespace std;
 
 int main() {
 
-	FibonacciHeap<int> fh;	
-	FibNode<int> * arr[N];	
-	for (int i=0; i<N; i++) arr[i] = fh.push(10+i);
+    FibonacciHeap<int> fh;  
+    FibonacciHeap<int>::FibNode * arr[N];  
+    for (int i=0; i<N; i++) arr[i] = fh.push(10+i);
 
-	cout << fh.top() << endl;
-	fh.pop();
-	cout << fh.top() << endl;
-	fh.decreaseKey(arr[3], 2);
-	cout << fh.top() << endl;
-	fh.pop();
-	cout << fh.top() << endl;
+    cout << fh.top() << endl;
+    fh.pop();
+    cout << fh.top() << endl;
+    fh.decreaseKey(arr[3], 2);
+    cout << fh.top() << endl;
+    fh.pop();
+    cout << fh.top() << endl;
 
-	return 0;
+    FibonacciHeap<int>::FibNode * tmp = fh.topNode();
+    cout << tmp->getKey() << ' ' << tmp->getID() << endl;
+
+    fh.push(-100, 77);
+    tmp = fh.topNode();
+    cout << tmp->getKey() << ' ' << tmp->getID() << endl;
+
+    return 0;
 }
 
 ````
--- fibonacciheap.h
+++ fibonacciheap.h
@@ -1,14 +1,10 @@
 /*
 
 Author: Louis Ye
-
-ATTENTION:
-In order to decrease the key, you needs to maintain an array of FibNode<T> * yourself.
-For instance, you may record every FibNode<T> * returned by FibonacciHeap.push()
-For user's convenience,
-there is a integer field of getID() and setID() in each FinNode<T> object
+Date : December 2013
 
 */
+
 
 #ifndef FIBONACCIHEAP_H
 #define FIBONACCIHEAP_H
@@ -16,74 +12,78 @@ there is a integer field of getID() and setID() in each FinNode<T> object
 #include <iostream>
 #include <math.h>
 
-//------------------------------------ NODE ------------------------------------
-template<class T>
-class FibNode {
-public:
-	T getKey() { return key; }
-	void setKey(T k) { key = k; }
-	bool compare(FibNode<T> * other) {
-		return key < other->getKey();
-	}
 
-	int getID() { return id; }
-	void setID(int i) { id = i; }
-
-	FibNode<T> * getParent() { return parent; }
-	void setParent(FibNode<T> * p) { parent = p; }
-
-	FibNode<T> * getLeftSib() { return leftSib; }
-	void setLeftSib(FibNode<T> * p) { leftSib = p; }
-
-	FibNode<T> * getRightSib() { return rightSib; }
-	void setRightSib(FibNode<T> * p) { rightSib = p; }
-
-	FibNode<T> * getChild() { return child; }
-	void setChild(FibNode<T> * p) { child = p; }
-
-	void mark() { flag = true; }
-	void unmark() { flag = false; }
-	bool isMark() { return flag; }
-
-	int getRank() { return rank; }
-	void incRank() { rank += 1; }
-	void decRank() { rank -= 1; }
-
-	FibNode (T k, int i) {
-		key = k;
-		parent = NULL;
-		leftSib = NULL;
-		rightSib = NULL;
-		child = NULL;
-		flag = false;
-		rank = 0;
-		id = i;
-	}
-
-private:
-	T key;
-	FibNode<T> * parent;
-	FibNode<T> * leftSib;
-	FibNode<T> * rightSib;
-	FibNode<T> * child;
-	bool flag;
-	int rank;
-	int id;
-
-};
-
-
-//------------------------------------ HEAP ------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------ HEAP ------------------------------------------------
 template<class T>
 class FibonacciHeap {
 public:
+
+	//--------------------------------------------------------------------------------------
+	//------------------------------------ NODE --------------------------------------------
+	class FibNode {
+	public:
+		T getKey() { return key; }
+		void setKey(T k) { key = k; }
+		bool compare(FibNode * other) {
+			return key < other->getKey();
+		}
+
+		int getID() { return id; }
+		void setID(int i) { id = i; }
+
+		FibNode * getParent() { return parent; }
+		void setParent(FibNode * p) { parent = p; }
+
+		FibNode * getLeftSib() { return leftSib; }
+		void setLeftSib(FibNode * p) { leftSib = p; }
+
+		FibNode * getRightSib() { return rightSib; }
+		void setRightSib(FibNode * p) { rightSib = p; }
+
+		FibNode * getChild() { return child; }
+		void setChild(FibNode * p) { child = p; }
+
+		void mark() { flag = true; }
+		void unmark() { flag = false; }
+		bool isMark() { return flag; }
+
+		int getRank() { return rank; }
+		void incRank() { rank += 1; }
+		void decRank() { rank -= 1; }
+
+		FibNode (T k, int i) {
+			key = k;
+			parent = NULL;
+			leftSib = NULL;
+			rightSib = NULL;
+			child = NULL;
+			flag = false;
+			rank = 0;
+			id = i;
+		}
+
+	private:
+		T key;
+		FibNode * parent;
+		FibNode * leftSib;
+		FibNode * rightSib;
+		FibNode * child;
+		bool flag;
+		int rank;
+		int id;
+
+	};
+	//----------------------------------------------------------------------------------------
+	//----------------------------------------------------------------------------------------
+
 	bool empty() {
 		return rootEntry == NULL;
 	}
 
 
 	//.........................................................................
-	FibNode<T> * topNode() {
+	FibNode * topNode() {
 		return minNode;
 	}
 
@@ -99,8 +99,8 @@ public:
 		if (empty()) return;
 
 
-		FibNode<T> * cur = minNode->getChild();
-		FibNode<T> * next = NULL;
+		FibNode * cur = minNode->getChild();
+		FibNode * next = NULL;
 		while (cur != NULL) {
 			next = cur->getRightSib();
 			addToRootList(cur);
@@ -115,7 +115,7 @@ public:
 
 
 		int mRank = (int)log2((double)heapSize) + 1;
-		FibNode<T> * ranks[mRank];
+		FibNode * ranks[mRank];
 		for (int i=0; i<mRank; i++) ranks[i] = NULL;
 		cur = rootEntry;
 		next = cur->getRightSib();
@@ -137,16 +137,30 @@ public:
 
 
 	//.........................................................................
-	FibNode<T> * push(T k) {
+	FibNode * push(T k) {
 		if (empty()) {
-			rootEntry = new FibNode<T>(k, totalNum++);
+			rootEntry = new FibNode(k, totalNum++);
 			minNode = rootEntry;
 			heapSize = 1;
 		}
 		else {
-			addToRootList(new FibNode<T>(k, totalNum++));
+			addToRootList(new FibNode(k, totalNum++));
 			heapSize += 1;
 		}
+		return rootEntry;
+	}
+
+	FibNode * push(T k, int id) {
+		if (empty()) {
+			rootEntry = new FibNode(k, id);
+			minNode = rootEntry;
+			heapSize = 1;
+		}
+		else {
+			addToRootList(new FibNode(k, id));
+			heapSize += 1;
+		}
+		totalNum++;
 		return rootEntry;
 	}
 
@@ -158,18 +172,18 @@ public:
 
 
 	//.........................................................................
-	void decreaseKey(FibNode<T> * nd, T k) {
+	void decreaseKey(FibNode * nd, T k) {
 		if (empty()) return;
 
 		//Judge if the new key is actually larger than the previous one
-		FibNode<T> tmp(k, -1);
+		FibNode tmp(k, -1);
 		if (nd->compare(&tmp)) return; //If yes, cancel the decreaseKey operation
 
 		//keep doing the decreaseKey operation
 		nd->setKey(k);
 		if (nd->compare(minNode)) minNode = nd;
 		if (nd->getParent() != NULL) {
-			FibNode<T> * par = nd->getParent();
+			FibNode * par = nd->getParent();
 			if (nd->compare(par)) { //If nd is smaller than the parent
 				deleteFromParent(nd, par);
 			}
@@ -179,9 +193,9 @@ public:
 
 	void printHeap() { //For testing
 
-		FibNode<T> * allnodes[heapSize];
-		FibNode<T> * cur = rootEntry;
-		FibNode<T> * next = NULL;
+		FibNode * allnodes[heapSize];
+		FibNode * cur = rootEntry;
+		FibNode * next = NULL;
 		int f=0, s=0;
 
 		printf("---------------------heapSize=%d-------------------------------\n", heapSize);
@@ -192,8 +206,8 @@ public:
 			allnodes[s++] = cur;
 			printf("#########\nroot : (key=%d, id=%d, rank=%d)\n", cur->getKey(), cur->getID(), cur->getRank());
 			while (f < s) {
-				FibNode<T> * father = allnodes[f];
-				FibNode<T> * son = father->getChild();
+				FibNode * father = allnodes[f];
+				FibNode * son = father->getChild();
 				while (son != NULL) {
 					printf("father:(key=%d, id=%d, rank=%d), son:(key=%d, id=%d, rank=%d)\n",father->getKey(), father->getID(), father->getRank(), son->getKey(), son->getID(), son->getRank());
 					allnodes[s++] = son;
@@ -216,17 +230,17 @@ public:
 	}
 
 	~FibonacciHeap() {
-		FibNode<T> * allnodes[heapSize];
-		FibNode<T> * cur = rootEntry;
-		FibNode<T> * next = NULL;
+		FibNode * allnodes[heapSize];
+		FibNode * cur = rootEntry;
+		FibNode * next = NULL;
 		int f=0, s=0;
 
 		while (cur != NULL) {
 			next = cur->getRightSib(); 
 			allnodes[s++] = cur;
 			while (f < s) {			//Do DFS for each tree
-				FibNode<T> * father = allnodes[f];
-				FibNode<T> * son = father->getChild();
+				FibNode * father = allnodes[f];
+				FibNode * son = father->getChild();
 				while (son != NULL) {
 					allnodes[s++] = son;
 					son = son->getRightSib();
@@ -242,12 +256,12 @@ public:
 	int static const MAX_RANK = 32;
 
 private:
-	FibNode<T> * rootEntry;
-	FibNode<T> * minNode;
+	FibNode * rootEntry;
+	FibNode * minNode;
 	int heapSize;
 	int totalNum;
 
-	void addToRootList(FibNode<T> * nd) {
+	void addToRootList(FibNode * nd) {
 		nd->setLeftSib(NULL);
 		nd->setRightSib(rootEntry);
 		rootEntry->setLeftSib(nd);
@@ -255,19 +269,19 @@ private:
 		if ( rootEntry->compare(minNode) ) minNode = rootEntry;
 	}
 
-	void deleteFromRootList(FibNode<T> * nd) {
-		FibNode<T> * left = nd->getLeftSib();
-		FibNode<T> * right = nd->getRightSib();
+	void deleteFromRootList(FibNode * nd) {
+		FibNode * left = nd->getLeftSib();
+		FibNode * right = nd->getRightSib();
 		if (rootEntry == nd) rootEntry = right;
 		if (left != NULL) left->setRightSib(right);
 		if (right != NULL) right->setLeftSib(left);
 	}
 
-	void deleteFromParent(FibNode<T> * nd, FibNode<T> * par) {
+	void deleteFromParent(FibNode * nd, FibNode * par) {
 		nd->setParent(NULL);
 		par->decRank();
-		FibNode<T> * left = nd->getLeftSib();
-		FibNode<T> * right = nd->getRightSib();
+		FibNode * left = nd->getLeftSib();
+		FibNode * right = nd->getRightSib();
 
 		if (par->getChild() == nd) par->setChild(right); //nd is the first child
 		if (left != NULL) left->setRightSib(right);
@@ -276,18 +290,18 @@ private:
 		addToRootList(nd);
 
 		if (par->isMark()) {
-			FibNode<T> * grandPar = par->getParent();
+			FibNode * grandPar = par->getParent();
 			par->unmark();
 			if (grandPar != NULL) deleteFromParent(par, grandPar);
 		}
 		else par->mark();
 	}
 
-	FibNode<T> * merge(FibNode<T> * a, FibNode<T> * b) {
-		FibNode<T> * big = a->compare(b) ? b : a;
-		FibNode<T> * sma = a->compare(b) ? a : b;
+	FibNode * merge(FibNode * a, FibNode * b) {
+		FibNode * big = a->compare(b) ? b : a;
+		FibNode * sma = a->compare(b) ? a : b;
 		deleteFromRootList(big);
-		FibNode<T> * child = sma->getChild();
+		FibNode * child = sma->getChild();
 		sma->setChild(big);
 		sma->incRank();
 		big->setParent(sma);
@@ -297,5 +311,7 @@ private:
 	}
 
 };
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
 
 #endif
